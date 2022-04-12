31a62ec71fe60be29507a4f4d1dc6b69562cdf4c
Piotr Ulanowski <pioula111@gmail.com>
2022-04-12 13:31:38 +0200

    default layout

---
+++ app.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.status(200).render('index.pug');
+})
+
+app.use((req, res, next) => {
+  next(404);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err).render('error', { error: err });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
