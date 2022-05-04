a53292e1a2ac1ffc59cc2ad8b0f6cff6003d6203
Piotr Ulanowski <pioula111@gmail.com>
2022-05-04 10:02:19 +0200

    without paging

--- controllers/home.controller.mjs
+++ controllers/home.controller.mjs
@@ -1,11 +1,10 @@
 import trips from "../public/javascripts/trips.mjs";
 import promotions from "../public/javascripts/promotions.mjs";
 import advices from "../public/javascripts/advices.mjs";
-import { page } from "../public/javascripts/home/pagination.mjs";
 
 const homeController = {
   get: (req, res) => {
-    res.status(200).render('index.pug', { trips: trips, promotions: promotions, advices: advices, page: page });
+    res.status(200).render('index.pug', { trips: trips, promotions: promotions, advices: advices });
   }
 }
 
